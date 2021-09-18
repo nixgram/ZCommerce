9c5eb730a372aef9bf35f861204e5a31e0b0eeca
sefatanam <sefatanam@gmail.com>
2021-09-18 16:09:01 +0600

    fix : remove unused models

--- src/ZCommerce.Api/Controllers/WeatherForecastController.cs
+++ src/ZCommerce.Api/Controllers/WeatherForecastController.cs
@@ -36,4 +36,15 @@ namespace ZCommerce.Api.Controllers
                 .ToArray();
         }
     }
+    
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
 }
