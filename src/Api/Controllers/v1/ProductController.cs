ad0d28189f232eed6bedb23f5e7d99f9c18ca76d
sefatanam <sefatanam@gmail.com>
2021-12-08 01:03:56 +0600

    feat : 1. product get query & handler 2. Authorize product controller

--- src/Api/Controllers/v1/ProductController.cs
+++ src/Api/Controllers/v1/ProductController.cs
@@ -1,19 +1,39 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Api.Controllers.Common;
+using Application.Common.Interfaces;
 using Application.Product.Commands;
+using Application.Product.Queries;
 using Application.Product.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers.v1
 {
+    [Authorize]
     public class ProductController : ApiBaseControllerV1
     {
+        private readonly ICurrentUserService _currentUserService;
+
+        public ProductController(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         public async Task<ActionResult<ProductVm>> Create(CreateProductCommand command)
         {
             return await Mediator.Send(command);
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<ProductVm>>> Get()
+        {
+            return await Mediator.Send(new GetAllProductsQuery {UserId = _currentUserService.UserId});
         }
     }
 }
---
+++ src/Application/Product/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Product.ViewModels;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Product.Queries
+{
+    public class GetAllProductsQuery : IRequest<List<ProductVm>>
+    {
+        public string UserId { get; set; }
+    }
+
+
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, List<ProductVm>>
+    {
+        private readonly IApplicationContext _context;
+        private readonly IMapper _mapper;
+
+        public GetAllProductsQueryHandler(IApplicationContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        public async Task<List<ProductVm>> Handle(GetAllProductsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var productList = await _context.Products.Where(d => d.CreatedBy == request.UserId).AsQueryable()
+                .ToListAsync(cancellationToken);
+
+            var productVms = _mapper.Map<List<ProductVm>>(productList);
+            return productVms;
+        }
+    }
+}
